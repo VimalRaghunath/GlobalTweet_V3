0e13e4aa332548e105bc3a31c2076ce2ff408345
VimalRaghunath <vimalvrk@gmail.com>
2023-11-23 17:37:58 +0530

    logout button

---
+++ frontend/src/Components/Home/logout.js
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function logout() {
+  return (
+    <div>
+      <Button>Logout</Button>
+    </div>
+  )
+}
+
+export default logout

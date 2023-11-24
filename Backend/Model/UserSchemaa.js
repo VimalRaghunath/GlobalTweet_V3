772c6e33587cb7913fd90c7c2a14fe25303d47dc
VimalRaghunath <vimalvrk@gmail.com>
2023-11-24 14:32:06 +0530

    just updation

--- Backend/Model/UserSchemaa.js
+++ Backend/Model/UserSchemaa.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-    name: { type: String, required: true},
-    email: { type: String, required: true},
-    mobile: { type: String, required: true},
-    username: { type: String, required: true},
-    password: { type: String, required: true},
-    id: { type: String, required: true},
-    bio: { type: String, required: true},
-    avatar : { type: String, required: true},
-    following: { type: Array, required: true},
-    followers:{ type: Array, required: true} ,
+    name: String,
+    email: String,
+    mobile: String,
+    username: String,
+    password: String,
+    id: String,
+    bio: String,
+    avatar : String,
+    following: Array,
+    followers: Array,
+    //   { type: Array, required: true} ,
     isBlocked: { type: Boolean , default:false},
 })
 

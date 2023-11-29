4a007af7a4faee1acb7bdaff5270f65bfa7b1dbe
VimalRaghunath <vimalvrk@gmail.com>
2023-11-29 10:24:11 +0530

    notification working code just created

--- frontend/src/Components/Home/Notifications.js
+++ frontend/src/Components/Home/Notifications.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar/Sidebar'
 import "./Notifications.css"
+import { Socket } from 'socket.io-client'
+import io from 'socket.io-client'
+import { Button } from '@mui/material'
 
-function Notifications() {
+const Notifications = () => {
+  
+  const [notificationData, setNotificationData] = useState('')
+  
+ const sendNotification = () => {
+  Socket.emit('notification',notificationData)
+ }
+
   return (
 
     <div className='Notifications'>
 
       <Sidebar/>
+
+      <Button onClick={sendNotification}>Send Notification</Button>
      
 
     </div>

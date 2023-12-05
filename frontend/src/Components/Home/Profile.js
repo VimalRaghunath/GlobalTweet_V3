c988b34099014d5099e502b4b0dd173cafa391bc
VimalRaghunath <vimalvrk@gmail.com>
2023-12-05 11:08:00 +0530

    cloudinary avatar setting up

--- frontend/src/Components/Home/Profile.js
+++ frontend/src/Components/Home/Profile.js
@@ -95,7 +95,7 @@ function Profile() {
         <div className="Profilecontent">
           <div className="Profileheader">
               <Avatar
-                src="https://images.unsplash.com/photo-1559065188-2537766d864b?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src=""
                 className="Profileavatar"
               />
             <div className="Profiletitles">

f8787c4c18f5ad46651d523cc379f22c6778ae87
VimalRaghunath <vimalvrk@gmail.com>
2023-12-05 10:08:31 +0530

    daily updation

--- Backend/Model/LikeSchema.js
+++ Backend/Model/LikeSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const LikeSchema = new mongoose.Schema({
     userId: { type : mongoose.Schema.Types.ObjectId, ref: "User"}
-
+    //post specifications,
 })
 
 module.exports = mongoose.model("Like",LikeSchema)
--- Backend/Model/PostSchema.js
+++ Backend/Model/PostSchema.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose")
 const PostSchema = new mongoose.Schema({
 
     userId: { type : mongoose.Schema.Types.ObjectId, ref: "User"},
-    title: { type: String,},
+    title: { type: String, },
     description: { type: String},
     image: { type: String, required: true},
     category: { type: String, },
-    likes: { type: String},
+    likes: { type: String}, //like details about users,count
     
 })
 
